ff0556a6f7695e38f43e22c59cbe0aeebae23e4f
onenewcode <lovestudy@qq.com>
2025-02-14 11:26:14 +0800

    t

--- exercises/easy/algorithm7.rs
+++ exercises/easy/algorithm7.rs
@@ -1,4 +1,3 @@
-
 /*
 	stack
 	This question requires you to use a stack to achieve a bracket match
@@ -10,7 +9,6 @@ struct Stack<T> {
     size: usize,
     data: Vec<T>,
 }
-
 impl<T> Stack<T> {
     fn new() -> Self {
         Self {
@@ -18,69 +16,113 @@ impl<T> Stack<T> {
             data: Vec::new(),
         }
     }
-
     fn is_empty(&self) -> bool {
-        self.size == 0
+        0 == self.size
     }
-
     fn len(&self) -> usize {
         self.size
     }
-
+    fn clear(&mut self) {
+        self.size = 0;
+        self.data.clear();
+    }
     fn push(&mut self, val: T) {
         self.data.push(val);
         self.size += 1;
     }
-
     fn pop(&mut self) -> Option<T> {
-        if self.is_empty() {
-            None
-        } else {
-            self.size -= 1;
-            self.data.pop()
-        }
+        self.size -= 1;
+        self.data.pop()
     }
-
     fn peek(&self) -> Option<&T> {
-        if self.is_empty() {
+        if 0 == self.size {
+            return None;
+        }
+        self.data.get(self.size - 1)
+    }
+    fn peek_mut(&mut self) -> Option<&mut T> {
+        if 0 == self.size {
+            return None;
+        }
+        self.data.get_mut(self.size - 1)
+    }
+    fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+    fn iter(&self) -> Iter<T> {
+        let mut iterator = Iter {
+            stack: Vec::new()
+        };
+        for item in self.data.iter() {
+            iterator.stack.push(item);
+        }
+        iterator
+    }
+    fn iter_mut(&mut self) -> IterMut<T> {
+        let mut iterator = IterMut {
+            stack: Vec::new()
+        };
+        for item in self.data.iter_mut() {
+            iterator.stack.push(item);
+        }
+        iterator
+    }
+}
+struct IntoIter<T>(Stack<T>);
+impl<T: Clone> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        if !self.0.is_empty() {
+            self.0.size -= 1;self.0.data.pop()
+        }
+        else {
             None
-        } else {
-            self.data.last()
         }
     }
 }
+struct Iter<'a, T: 'a> {
+    stack: Vec<&'a T>,
+}
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.stack.pop()
+    }
+}
+struct IterMut<'a, T: 'a> {
+    stack: Vec<&'a mut T>,
+}
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.stack.pop()
+    }
+}
 
-fn bracket_match(bracket: &str) -> bool {
-    let mut stack = Stack::new();
+fn bracket_match(bracket: &str) -> bool
+{
+    let mut stack:Stack<u8> = Stack::new();
 
-    // Define the matching pairs
-    let matching_pairs = |open: char, close: char| -> bool {
-        match open {
-            '(' => close == ')',
-            '{' => close == '}',
-            '[' => close == ']',
-            _ => false,
-        }
-    };
-
-    for c in bracket.chars() {
-        // If it's an opening bracket, push it to the stack
-        if c == '(' || c == '{' || c == '[' {
-            stack.push(c);
-        }
-        // If it's a closing bracket, check if it matches the top of the stack
-        else if c == ')' || c == '}' || c == ']' {
-            if let Some(top) = stack.pop() {
-                if !matching_pairs(top, c) {
-                    return false; // If there's no match, return false
-                }
-            } else {
-                return false; // If the stack is empty but a closing bracket is found
+    let brackets = bracket.as_bytes();
+    for bracket in brackets.iter() {
+        if *bracket == b'(' || *bracket == b'[' || *bracket == b'{' {
+            stack.push(*bracket);
+        } else if *bracket == b')' || *bracket == b']' || *bracket == b'}' {
+            match *bracket {
+                b')' => if stack.is_empty() || stack.pop().unwrap() != b'(' {
+                    return false;
+                },
+                b']' => if stack.is_empty() || stack.pop().unwrap() != b'[' {
+                    return false;
+                },
+                b'}' => if stack.is_empty() || stack.pop().unwrap() != b'{' {
+                    return false;
+                },
+                _ => panic!("it's not a bracket!"),
             }
         }
     }
 
-    // If the stack is empty at the end, all brackets matched
     stack.is_empty()
 }
 
@@ -89,38 +131,33 @@ mod tests {
     use super::*;
 
     #[test]
-    fn bracket_matching_1() {
+    fn bracket_matching_1(){
         let s = "(2+3){func}[abc]";
-        assert_eq!(bracket_match(s), true);
+        assert_eq!(bracket_match(s),true);
     }
-
     #[test]
-    fn bracket_matching_2() {
+    fn bracket_matching_2(){
         let s = "(2+3)*(3-1";
-        assert_eq!(bracket_match(s), false);
+        assert_eq!(bracket_match(s),false);
     }
-
     #[test]
-    fn bracket_matching_3() {
+    fn bracket_matching_3(){
         let s = "{{([])}}";
-        assert_eq!(bracket_match(s), true);
+        assert_eq!(bracket_match(s),true);
     }
-
     #[test]
-    fn bracket_matching_4() {
+    fn bracket_matching_4(){
         let s = "{{(}[)]}";
-        assert_eq!(bracket_match(s), false);
+        assert_eq!(bracket_match(s),false);
     }
-
     #[test]
-    fn bracket_matching_5() {
+    fn bracket_matching_5(){
         let s = "[[[]]]]]]]]]";
-        assert_eq!(bracket_match(s), false);
+        assert_eq!(bracket_match(s),false);
     }
-
     #[test]
-    fn bracket_matching_6() {
+    fn bracket_matching_6(){
         let s = "";
-        assert_eq!(bracket_match(s), true);
+        assert_eq!(bracket_match(s),true);
     }
 }
